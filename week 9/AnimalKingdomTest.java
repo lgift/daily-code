8abcb3710389adf5d8c172b23ec0faf352e27e88
lgift <lpgift@my.waketech.edu>
2025-04-14 10:05:44 -0400

    asdfghjkll

--- week 9/AnimalKingdomTest.java
+++ week 9/AnimalKingdomTest.java
@@ -1,45 +1,18 @@
-// Base class @
-// TODO: Define a class called Animal with: @
-// - A protected String field called name @
-// - A constructor that takes a name and assigns it @
-// - A public void method called speak() that prints a generic sound @
-// - Override toString() to return "Animal: " + name @
-// - Override equals(Object o) to compare names if o is an Animal @
+class AnimalKingdomTest {
+    public static void main(String[] args) {
+        Animal a1 = new Dog("Buddy");
+        Animal a2 = new Cat("Whiskers");
 
-// Subclass
-// TODO: Define a class called Mammal that extends Animal with: @
-// - A protected boolean field called warmBlooded (set to true) @
-// - A protected String field called dietType (e.g., "herbivore") @
-// - A protected int field called numLegs (e.g., 4) @
-// - A protected String field called sound (e.g., "growl") @
-// - A constructor that uses super to set name and sets these fields @ 
-// - Override speak() to print the value of sound prefixed by name @ 
+        a1.speak();
+        a2.speak();
 
-// Subclasses of Mammal
-// TODO: Define a class Dog that extends Mammal @
-// - Constructor takes name and passes name, true, "omnivore", 4, "Woof!" to super @
-// - Override speak() to print "<name> says: Woof!" @
+        System.out.println(a1.equals(new Dog("Buddy")));
 
-// TODO: Define a class Cat that extends Mammal @
-// - Constructor takes name and passes name, true, "carnivore", 4, "Meow!" to super @
-// - Override speak() to print "<name> says: Meow!" @
-
-// TODO: Define a final class Human that extends Mammal @
-// - Constructor takes name and passes name, true, "omnivore", 2, "Hello!" to super @
-
-// TODO: Define a class Reptile that extends Animal @
-// - Add fields: boolean warmBlooded (set to false), int numLegs, String dietType, String sound @
-// - Constructor should take all values and use super for name @
-// - Override speak() to print the value of sound prefixed by name @
-
-// Main class to test
-// TODO: Define class AnimalKingdomTest with a main method @
-// - Create Animal a1 = new Dog("Buddy") @
-// - Create Animal a2 = new Cat("Whiskers") @
-// - Call speak() on both (demonstrating polymorphism) @
-// - Print both using System.out.println @
-// - Use equals() to compare a1 to a new Dog("Buddy") @
-
+        Animal yo = new Animal("joe");
+        Mammal yo1 = (Mammal)yo;
+        yo1.performTrick();
+    }
+}
 
 class Animal {
     protected String name;
@@ -82,7 +55,7 @@ class Mammal extends Animal {
         this.sound = sound;
     }
 
-    performTrick() {
+    public void performTrick() {
         System.out.println("EPIC TRICK SUPER EPIC TRICK AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
     }
     
@@ -148,22 +121,6 @@ class Reptile extends Animal {
     
 }
 
-class AnimalKingdomTest {
-    public static void main(String[] args) {
-        Animal a1 = new Dog("Buddy");
-        Animal a2 = new Cat("Whiskers");
-
-        a1.speak();
-        a2.speak();
-
-        System.out.println(a1.equals(new Dog("Buddy")));
-
-        Animal yo = new Animal("joe");
-        Mammal yo1 = (Mammal)yo;
-        yo1.performTrick();
-    }
-}
-
 class Bird extends Animal {
 
     boolean warmBlooded = true;
@@ -198,9 +155,3 @@ class Bird extends Animal {
 // }
 // -----------------------------------------
 
-
-// Additional TODOs:
-// 1. Create a new subclass of Animal (e.g., Bird) and override speak() @
-// 2. Try to extend Human – what happens and why? @
-// 3. Override equals() in Dog or Cat to also check the type @ 
-// 4. Add a method to Mammal called performTrick() and call it via downcasting
